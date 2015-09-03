311a4ab99565f0f0f48c351821c86929194b247c
Rafael Dantas Justo <rafael@registro.br>
2015-09-03 10:24:05 -0300

    Fix file permissions and apparmor

--- puppet/modules/bind9/manifests/init.pp
+++ puppet/modules/bind9/manifests/init.pp
@@ -7,7 +7,7 @@ class bind9 {
     ensure  => "directory",
     require => Package["bind9"],
     owner   => "qualificati",
-    group   => "qualificati",
+    group   => "bind",
     mode    => 750,
   }
 
@@ -18,12 +18,42 @@ class bind9 {
       File["/home/qualificati/etc/bind"],
     ],
     source  => "puppet:///modules/bind9/named.conf",
+    owner   => "qualificati",
+    group   => "bind",
+    mode    => 644,
   }
 
   file { "/etc/bind/named.conf":
     ensure  => link,
     require => File["/home/qualificati/etc/bind/named.conf"],
     target  => "/home/qualificati/etc/bind/named.conf",
+    owner   => "root",
+    group   => "bind",
+  }
+
+  file { "/var/log/named":
+    ensure  => "directory",
+    require => Package["bind9"],
+    owner   => "root",
+    group   => "bind",
+    mode    => 775,
+  }
+
+  file { "/etc/apparmor.d/usr.sbin.named":
+    ensure  => file,
+    require => File["/etc/bind/named.conf"],
+    source  => "puppet:///modules/bind9/usr.sbin.named",
+    notify  => Service["apparmor"],
+    owner   => "root",
+    group   => "root",
+    mode    => 644,
+  }
+
+  service { "apparmor":
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
   }
 
   service { "bind9":
@@ -31,6 +61,10 @@ class bind9 {
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
-    subscribe  => [ File["/etc/bind/named.conf"] ],
+    subscribe  => [
+      File["/etc/bind/named.conf"],
+      File["/var/log/named"],
+      File["/etc/apparmor.d/usr.sbin.named"],
+    ],
   }
 }
